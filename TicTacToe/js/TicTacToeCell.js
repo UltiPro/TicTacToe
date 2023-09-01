6a024b494af8474003e8de907df6dfa50a77485e
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-09-01 22:30:13 +0200

    Update TicTacToeCell.js

--- TicTacToe/js/TicTacToeCell.js
+++ TicTacToe/js/TicTacToeCell.js
@@ -1,33 +1,30 @@
 export class TicTacToeCell {
-    //tutaj
-    #domElement;
-    #seted;
+    #elementDiv;
+    #value;
 
-    constructor(idx, gameData) {
-        this.#domElement = document.createElement("div");
-        this.#domElement.classList.add("square");
-        this.#domElement.addEventListener("click", () => { this.Set(idx, gameData); });
-        this.#domElement.appendChild(document.createElement("div"));
-        this.#seted = null;
+    constructor(gameBox, gameData) {
+        this.#elementDiv = document.createElement("div");
+        this.#elementDiv.classList.add("square");
+        this.#elementDiv.addEventListener("click", () => { this.#Click(gameData); });
+        this.#elementDiv.appendChild(document.createElement("div"));
+        gameBox.append(this.#elementDiv);
+        this.#value = null;
     }
 
-    Get = () => this.#domElement;
+    get Value() { return this.#value; }
 
-    Set = (idx, gameData) => {
-        if (this.#seted === true || this.#seted === false || !gameData.gameStarted) return;
-        if (gameData.crossTurn) this.#domElement.children[0].classList.add("cross");
-        else this.#domElement.children[0].classList.add("circle");
-        this.#seted = gameData.crossTurn;
+    #Click(gameData) {
+        if (this.#value === true || this.#value === false || !gameData.gameStarted) return;
+        if (gameData.crossTurn) this.#elementDiv.children[0].classList.add("cross");
+        else this.#elementDiv.children[0].classList.add("circle");
+        this.#value = gameData.crossTurn;
         gameData.crossTurn = !gameData.crossTurn;
         gameData.event();
     }
 
-    Clear = () => {
-        this.#domElement.children[0].classList.remove("cross");
-        this.#domElement.children[0].classList.remove("circle");
-        this.#seted = null;
+    Clear() {
+        this.#elementDiv.children[0].classList.remove("cross");
+        this.#elementDiv.children[0].classList.remove("circle");
+        this.#value = null;
     }
-
-    Value = () => this.#seted;
-    //tutaj
 }

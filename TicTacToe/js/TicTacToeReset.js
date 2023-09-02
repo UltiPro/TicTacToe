dface655ce526f289adbf77c99539d6efef3f357
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-09-02 20:41:11 +0200

    Update TicTacToeReset.js

--- TicTacToe/js/TicTacToeReset.js
+++ TicTacToe/js/TicTacToeReset.js
@@ -12,8 +12,8 @@ export class TicTacToeReset {
         const resetBox = document.createElement("div");
         resetBox.classList.add("reset-box");
         resetBox.innerHTML = `
-            <button class="button-small" disabled>Play Again</button>
-            <button class="button-small">Change Mode</button>
+            <button class="button-small button-slow" disabled>Play Again</button>
+            <button class="button-small">Reset Game</button>
         `;
         this.#mainDiv.append(resetBox);
         this.#playAgainBtn = document.querySelector(".reset-box button:nth-child(1)");
@@ -22,15 +22,21 @@ export class TicTacToeReset {
         });
     }
 
-    #ClickPlayAgain = () => this.#ticTacToe.TicTacToeGame.Reset();
+    #ClickPlayAgain = () => this.#ticTacToe.TicTacToeGame.ResetGame();
 
     ActivePlayAgain() {
         this.#playAgainBtn.removeAttribute("disabled");
         this.#playAgainBtn.addEventListener("click", this.#ClickPlayAgain);
+        setTimeout(function () {
+            this.#playAgainBtn.classList.remove("button-slow");
+        }.bind(this), 1200);
     }
 
     DisablePlayAgain() {
         this.#playAgainBtn.setAttribute("disabled", "");
         this.#playAgainBtn.removeEventListener("click", this.#ClickPlayAgain);
+        setTimeout(function () {
+            this.#playAgainBtn.classList.add("button-slow");
+        }.bind(this), 1200);
     }
 }

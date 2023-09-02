76a55601003d8cddd9ada09c967570ea1fb5b4ef
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-09-03 01:47:56 +0200

    Update TicTacToeCell.js

--- TicTacToe/js/TicTacToeCell.js
+++ TicTacToe/js/TicTacToeCell.js
@@ -1,28 +1,43 @@
 export class TicTacToeCell {
     #elementDiv;
+    #gameData;
     #value;
 
     constructor(gameBox, gameData) {
         this.#elementDiv = document.createElement("div");
         this.#elementDiv.classList.add("square");
-        this.#elementDiv.addEventListener("click", () => { this.#Click(gameData); });
+        this.#elementDiv.addEventListener("click", () => { this.#Click(); });
         this.#elementDiv.appendChild(document.createElement("div"));
         gameBox.append(this.#elementDiv);
+        this.#gameData = gameData;
         this.#value = null;
     }
 
     get Value() { return this.#value; }
 
-    #Click(gameData) {
-        if (this.#value === true || this.#value === false || !gameData.gameStarted) return;
-        if (gameData.crossTurn) this.#elementDiv.children[0].classList.add("cross");
+    #Click() {
+        if (this.#value === true || this.#value === false || !this.#gameData.gameStarted) return;
+        if (this.#gameData.crossTurn) this.#elementDiv.children[0].classList.add("cross");
         else this.#elementDiv.children[0].classList.add("circle");
-        this.#value = gameData.crossTurn;
-        gameData.crossTurn = !gameData.crossTurn;
-        gameData.event();
+        this.#value = this.#gameData.crossTurn;
+        this.#gameData.crossTurn = !this.#gameData.crossTurn;
+        this.#gameData.event();
+    }
+
+    ClickByComputer() {
+        if (!this.#gameData.gameStarted) return;
+        if (this.#gameData.crossTurn) this.#elementDiv.children[0].classList.add("cross");
+        else this.#elementDiv.children[0].classList.add("circle");
+        this.#value = this.#gameData.crossTurn;
+        this.#gameData.crossTurn = !this.#gameData.crossTurn;
+    }
+
+    SetActive() {
+        this.#elementDiv.classList.add("square-active");
     }
 
     Clear() {
+        this.#elementDiv.classList.remove("square-active");
         this.#elementDiv.children[0].classList.remove("cross");
         this.#elementDiv.children[0].classList.remove("circle");
         this.#value = null;

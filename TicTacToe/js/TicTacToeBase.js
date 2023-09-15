285ee1421e5c6c92e635b823ac5fc0e75be5b890
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-09-15 23:50:52 +0200

    Update TicTacToeBase.js

--- TicTacToe/js/TicTacToeBase.js
+++ TicTacToe/js/TicTacToeBase.js
@@ -2,7 +2,7 @@ export class TicTacToeBase {
     #inited;
 
     constructor() {
-        if (this.constructor == TicTacToeBase)
+        if (this.constructor === TicTacToeBase)
             throw new Error("Abstract classes can't be instantiated.");
         this.#inited = false;
     }
